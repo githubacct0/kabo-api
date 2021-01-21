e74e415f546adfb8edcb6ebb809e0dc8b4aa130c
remy727 <remberth727@gmail.com>
2021-01-21 05:24:59 -0500

    Drop users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_18_122832) do
+ActiveRecord::Schema.define(version: 2021_01_21_102016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", null: false
-    t.string "password_digest", null: false
-    t.datetime "created_at", precision: 6, default: -> { "now()" }, null: false
-    t.datetime "updated_at", precision: 6, default: -> { "now()" }, null: false
-  end
 
 end
