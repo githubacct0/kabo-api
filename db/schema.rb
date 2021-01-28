a1f0553b453e671a5db392171bc7db57d1f99a89
remy727 <remberth727@gmail.com>
2021-01-28 02:52:54 -0500

    Add lamb_recipe column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_22_081419) do
+ActiveRecord::Schema.define(version: 2021_01_28_075115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2021_01_22_081419) do
     t.integer "kibble_portion"
     t.datetime "created_at", precision: 6, default: -> { "CURRENT_TIMESTAMP" }, null: false
     t.datetime "updated_at", precision: 6, default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.boolean "lamb_recipe", default: false
     t.index ["user_id"], name: "index_dogs_on_user_id"
   end
 
