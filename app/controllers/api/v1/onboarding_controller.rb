b94764e70c6dd0135e4c672178a7769c5c3bdbfa
remy727 <remberth727@gmail.com>
2021-02-19 08:23:24 -0500

    Fix: add params validation

--- app/controllers/api/v1/onboarding_controller.rb
+++ app/controllers/api/v1/onboarding_controller.rb
@@ -116,108 +116,119 @@ class Api::V1::OnboardingController < ActionController::API
     end
   end
 
-  # Update
+  # Route: /api/v1/onboarding/users/:user_id
+  # Method: PUT
+  # Update user and dogs
   def update
-    temp_user = TempUser.find(params[:user_id])
-    if temp_user.nil?
-      render json: {
-        error: "Temp User doesn't exist!"
-      }, status: :not_found
-    else
-      step = onboarding_params[:step]
-      update_params = {}
-
-      if ["detail", "recipes", "portions"].include?(step)
-        update_params[:plan_interval] = onboarding_params[:plan_interval] if step == "portions"
-        update_params[:temp_dogs_attributes] = onboarding_params[:dogs]
-        temp_user.update(update_params)
-
+    if onboarding_params_valid?
+      temp_user = TempUser.find_by_id(params[:user_id])
+      if temp_user.nil?
         render json: {
-          temp_user_id: temp_user.id,
-          temp_dog_ids: temp_user.temp_dog_ids
-        }, status: :ok
-      elsif step == "account"
-        onboarding_params[:first_name].present? && update_params[:first_name] = onboarding_params[:first_name]
-        onboarding_params[:email].present? && update_params[:email] = onboarding_params[:email]
-        email = update_params[:email]
-        if email.present?
-          email_valid = TempUser.is_email_valid?(email)
-        else
-          email_valid = true
-          update_params[:email] = "#{SecureRandom.uuid}-temp-user@kabo.co"
-        end
+          error: "Temp User doesn't exist!"
+        }, status: :not_found
+      else
+        step = onboarding_params[:step]
+        update_params = {}
 
-        if email_valid
-          checkout_token = SecureRandom.uuid
-          update_params[:checkout_token] = checkout_token
-          update_params[:chargebee_plan_interval] = "#{temp_user.calculated_trial_length}_weeks"
-          temp_user.update(update_params)
-
-          referral_code = onboarding_params[:referral_code]
-          referral_check = MyLib::Referral.check_code(referral_code)
-          applied_referral_code = referral_check ? "'#{referral_code}' used. #{referral_check} discount applied!" : "'40off' used. 40% discount applied!"
-
-          # TAG: CHECK IF NEED
-          if onboarding_params[:token].present?
-            agreement_response = MyLib::Paypal.create_billing_agreement
-
-            if Rack::Utils.parse_nested_query(agreement_response.body)["ACK"] == "Failure"
-              render json: {
-                error: "Cancelled PayPal Authorization"
-              }, status: :bad_request
-            else
-              details_response = MyLib::Paypal.get_express_checkout_details
-              paypal_checkout_details = Rack::Utils.parse_nested_query(details_response.body)
-              user = {
-                email: paypal_checkout_details["EMAIL"],
-                paypal_email: paypal_checkout_details["EMAIL"],
-                first_name: paypal_checkout_details["FIRSTNAME"],
-                shipping_last_name:  paypal_checkout_details["LASTNAME"],
-                shipping_street_address: paypal_checkout_details["SHIPTOSTREET"],
-                shipping_city: paypal_checkout_details["SHIPTOCITY"],
-                shipping_postal_code: paypal_checkout_details["SHIPTOZIP"].delete(" "),
-                reference_id: Rack::Utils.parse_nested_query(agreement_response.body)["BILLINGAGREEMENTID"]
-              }
-
-              temp_dogs = temp_user.dogs.map do |temp_dog|
-                {
-                  id: temp_dog.id,
-                  mean_plan: temp_dog.mean_plan,
-                  topper_available: temp_dog.topper_available,
-                  checkout_esimate: MyLib::Checkout.estimate_v2(temp_user, temp_dog, referral_code, referral_code, nil)
-                }
-              end
-
-              render json: {
-                temp_user_id: temp_user.id,
-                temp_dogs: temp_dogs,
-                user: user,
-                applied_referral_code: applied_referral_code,
-                paypal_checkout: true,
-                checkout_token: checkout_token,
-              }, status: :ok
-            end
-          else
-            temp_dogs = temp_user.temp_dogs.map { |temp_dog|
-              {
-                name: temp_dog.name,
-                meal_type: temp_dog.meal_type,
-                checkout_estimate: MyLib::Checkout.estimate_v2(temp_user, temp_dog, referral_code, nil, nil, true)
-              }
-            }
+        if ["detail", "recipes", "portions"].include?(step)
+          update_params[:plan_interval] = onboarding_params[:plan_interval] if step == "portions"
+          update_params[:temp_dogs_attributes] = onboarding_params[:dogs]
+          begin
+            temp_user.update!(update_params)
 
             render json: {
               temp_user_id: temp_user.id,
-              applied_referral_code: applied_referral_code,
-              temp_dogs: temp_dogs
+              temp_dog_ids: temp_user.temp_dog_ids
             }, status: :ok
+          rescue => e
+            puts "app_log(ERROR): #{e.message}"
+            render_error("", :not_found, true)
           end
-        else
-          render json: {
-            error: "Email is invalid!"
-          }, status: :bad_request
+        elsif step == "account"
+          onboarding_params[:first_name].present? && update_params[:first_name] = onboarding_params[:first_name]
+          onboarding_params[:email].present? && update_params[:email] = onboarding_params[:email]
+          email = update_params[:email]
+          if email.present?
+            email_valid = TempUser.is_email_valid?(email)
+          else
+            email_valid = true
+            update_params[:email] = "#{SecureRandom.uuid}-temp-user@kabo.co"
+          end
+
+          if email_valid
+            checkout_token = SecureRandom.uuid
+            update_params[:checkout_token] = checkout_token
+            update_params[:chargebee_plan_interval] = "#{temp_user.calculated_trial_length}_weeks"
+            temp_user.update(update_params)
+
+            referral_code = onboarding_params[:referral_code]
+            referral_check = MyLib::Referral.check_code(referral_code)
+            applied_referral_code = referral_check ? "'#{referral_code}' used. #{referral_check} discount applied!" : "'40off' used. 40% discount applied!"
+
+            # TAG: CHECK IF NEED
+            if onboarding_params[:token].present?
+              agreement_response = MyLib::Paypal.create_billing_agreement
+
+              if Rack::Utils.parse_nested_query(agreement_response.body)["ACK"] == "Failure"
+                render json: {
+                  error: "Cancelled PayPal Authorization"
+                }, status: :bad_request
+              else
+                details_response = MyLib::Paypal.get_express_checkout_details
+                paypal_checkout_details = Rack::Utils.parse_nested_query(details_response.body)
+                user = {
+                  email: paypal_checkout_details["EMAIL"],
+                  paypal_email: paypal_checkout_details["EMAIL"],
+                  first_name: paypal_checkout_details["FIRSTNAME"],
+                  shipping_last_name:  paypal_checkout_details["LASTNAME"],
+                  shipping_street_address: paypal_checkout_details["SHIPTOSTREET"],
+                  shipping_city: paypal_checkout_details["SHIPTOCITY"],
+                  shipping_postal_code: paypal_checkout_details["SHIPTOZIP"].delete(" "),
+                  reference_id: Rack::Utils.parse_nested_query(agreement_response.body)["BILLINGAGREEMENTID"]
+                }
+
+                temp_dogs = temp_user.dogs.map do |temp_dog|
+                  {
+                    id: temp_dog.id,
+                    mean_plan: temp_dog.mean_plan,
+                    topper_available: temp_dog.topper_available,
+                    checkout_esimate: MyLib::Checkout.estimate_v2(temp_user, temp_dog, referral_code, referral_code, nil)
+                  }
+                end
+
+                render json: {
+                  temp_user_id: temp_user.id,
+                  temp_dogs: temp_dogs,
+                  user: user,
+                  applied_referral_code: applied_referral_code,
+                  paypal_checkout: true,
+                  checkout_token: checkout_token,
+                }, status: :ok
+              end
+            else
+              temp_dogs = temp_user.temp_dogs.map { |temp_dog|
+                {
+                  name: temp_dog.name,
+                  meal_type: temp_dog.meal_type,
+                  checkout_estimate: MyLib::Checkout.estimate_v2(temp_user, temp_dog, referral_code, nil, nil, true)
+                }
+              }
+
+              render json: {
+                temp_user_id: temp_user.id,
+                applied_referral_code: applied_referral_code,
+                temp_dogs: temp_dogs
+              }, status: :ok
+            end
+          else
+            render json: {
+              error: "Email is invalid!"
+            }, status: :bad_request
+          end
         end
       end
+    else
+      render_missed_params
     end
   end
 
@@ -250,13 +261,31 @@ class Api::V1::OnboardingController < ActionController::API
     end
 
     def onboarding_params_valid?
-      return false if params[:step].nil?
+      step = params[:step]
+      return false if step.nil?
 
-      case params[:step]
-      when "start"
-        keys = [:name, :breed, :age_in_months]
+      if ["start", "detail", "recipes"].include? step
+        keys =
+          case step
+          when "start" then [:name, :breed, :age_in_months]
+          when "detail" then [:id, :gender, :neutered, :weight, :weight_unit, :body_type, :activity_level]
+          when "recipes" then [:id, :chicken_recipe, :beef_recipe, :turkey_recipe, :lamb_recipe, :kibble_recipe]
+          end
+
         onboarding_params[:dogs].present? &&
           onboarding_params[:dogs].all? { |dog| keys.all? { |key| dog.key? key } }
+      elsif step == "portions"
+        keys = ["cooked_portion", "kibble_portion"]
+        onboarding_params[:plan_interval].present? &&
+          onboarding_params[:dogs].present? &&
+          onboarding_params[:dogs].all? { |dog|
+            dog.key?("id") && (keys & dog.keys).any?
+          }
+      elsif step == "account"
+        onboarding_params[:first_name].present? &&
+          onboarding_params[:email].present?
+      else
+        false
       end
     end
 end
